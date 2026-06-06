07ca7ec580bbef14b8d673a3b72eb24f7bbcf5c5
yogeshkumawat2027 <yogeshkumawat2027@gmail.com>
2026-06-06 11:20:35 +0530

    refactor: update middleware imports for consistency

--- horoo-backend-v1/routes/uploadRoutes.js
+++ horoo-backend-v1/routes/uploadRoutes.js
@@ -2,9 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
-const auth = require("../middlewares/auth");
-const isOwner = require("../middlewares/isOwner");
+const { auth } = require("../middlewares/auth");
+
+const { isOwner} = require("../middlewares/isOwner");
+
 const { getUploadSignature } = require('../controllers/uploadController');
+
+// console.log(auth);
+// console.log(isOwner);
+// console.log(getUploadSignature);
 
 router.get("/signature" , auth ,isOwner , getUploadSignature);
 
