f2492021251bec03a8e0425ea4c0c50e233d09b2
yogeshkumawat2027 <yogeshkumawat2027@gmail.com>
2026-08-14 17:34:14 +0530

    add Footer component to enhance site navigation and information

--- horoo-frontend/app/page.tsx
+++ horoo-frontend/app/page.tsx
@@ -1,11 +1,13 @@
 
 import Hero from "@/components/layout/Hero";
+import Footer from "@/components/layout/Footer";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <>
    <Hero />
+   <Footer />
    </>
   );
 }
