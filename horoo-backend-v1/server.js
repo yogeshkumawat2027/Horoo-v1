4495dc0b928c1cc9400d53d4235c50f85c932591
yogeshkumawat2027 <yogeshkumawat2027@gmail.com>
2026-08-20 11:01:50 +0530

    added cors funtionality

--- horoo-backend-v1/server.js
+++ horoo-backend-v1/server.js
@@ -22,7 +22,12 @@ dotenv.config();
 const app = express();
 
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
